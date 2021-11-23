04d269464524f210cb88aaa4206e02f9094cf719
B4eva <tanzebright@gmail.com>
2021-11-23 09:13:35 +0100

    create master branch

--- server/rooms.go
+++ server/rooms.go
@@ -1,7 +1,8 @@
 package server
 
 import (
-	"fmt"
+	// "fmt"
+	"log"
 	"math/rand"
 	"sync"
 	"time"
@@ -9,11 +10,13 @@ import (
 	"github.com/gorilla/websocket"
 )
 
+// Participant describes a single entity in the hasMap
 type Participant struct {
 	Host bool
 	Conn *websocket.Conn
 }
 
+//RoomMap is the main hashMap [roomID string -> []Participants]
 type RoomMap struct {
 	Mutex sync.RWMutex
 	Map   map[string][]Participant
@@ -34,15 +37,40 @@ func (r *RoomMap) Get(roomID string) []Participant {
 
 }
 
-//CreateRoom generate a unique ID and returns it then  -> insert into the hashMap (RoomMap)
+//CreateRoom generate a unique ID and returns it then  -> insert into the hashMap [RoomMap]
 func (r *RoomMap) CreateRoom() string {
 	r.Mutex.Lock()
 	defer r.Mutex.Unlock()
 
 	rand.Seed(time.Now().UnixNano())
 	var letters = []rune("abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ1234567890")
-	fmt.Println(letters)
-	return string(letters)
+	b := make([]rune, 8)
+
+	for i := range b {
+		b[i] = letters[rand.Intn(len(letters))]
+	}
+
+	roomId := string(b)
+	r.Map[roomId] = []Participant{}
+
+	return roomId
+
 }
 
-func (r *RoomMap) DeleteRoom() {}
+// InsertIntoRoom will create a participant and add it in the hashmap
+func (r *RoomMap) InsertIntoRoom(roomID string, host bool, conn *websocket.Conn) {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	p := Participant{host, conn}
+
+	log.Println("inserting into Room with RoomID:", roomID)
+	r.Map[roomID] = append(r.Map[roomID], p)
+}
+
+func (r *RoomMap) DeleteRoom(roomID string) {
+	r.Mutex.Lock()
+	defer r.Mutex.Unlock()
+
+	delete(r.Map, roomID)
+}
